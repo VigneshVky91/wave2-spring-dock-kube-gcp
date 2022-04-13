8e0fed1494fd261508f6bedaba05de2330c83a61
VigneshVky91 <vigneshvky91@gmail.com>
2022-04-13 15:41:28 +0530

    First Commit

---
+++ src/main/java/com/stackroute/cgi/Wave2Controller.java
@@ -0,0 +1,12 @@
+package com.stackroute.cgi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Wave2Controller {
+	@GetMapping("/msg")
+	public String getMessage() {
+		return "Hello from IBM Wave 2 Batch";
+	}
+}
---
+++ src/test/java/com/stackroute/cgi/Wave2SpringWithDockerHubKubeGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.cgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wave2SpringWithDockerHubKubeGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
